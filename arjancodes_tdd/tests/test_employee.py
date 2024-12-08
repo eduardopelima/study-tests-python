4def3d6c5168bbccb8003866f350031b106b51d1
EDUARDO LIMA <weduardow123@gmail.com>
2024-12-07 23:10:48 -0300

    fix: workspace vscode file

--- arjancodes_tdd/tests/test_employee.py
+++ arjancodes_tdd/tests/test_employee.py
@@ -1,6 +1,6 @@
+from models.employee import Employee
 import unittest
 
-from arjancodes_tdd.models.employee import Employee
 
 NAME: str = "Gutierre"
 EMPLOYEE_ID: int = "1"
@@ -14,20 +14,26 @@ class TestEmployeeComputePayout(unittest.TestCase):
 
     def test_employee_payout_returns_a_float(self):
         """Wheter payout returns a float."""
-        raise NotImplementedError
+        self.assertIsInstance(self.gutierre.compute_payout(), float)
     
     def test_employee_payout_no_comission_no_hours(self):
         """Wheter payout is correctly computed in case of no comission and no hours worked."""
-        raise NotImplementedError
+        self.assertAlmostEqual(self.gutierre.compute_payout(), 1000.0)
     
     def test_employee_payout_no_comission(self):
         """Wheter payout is correctly computed in case of no comission and 10 hours worked."""
-        raise NotImplementedError
+        self.gutierre.hours_worked = 10
+        self.assertAlmostEqual(self.gutierre.compute_payout(), 1000.0)
     
     def test_employee_payout_with_comission(self):
         """Wheter payout is corretly computed in case of 10 contracts landed and 10 hours worked."""
-        raise NotImplementedError
+        self.gutierre.hours_worked = 10
+        self.gutierre.contracts_landed = 10
+        self.assertAlmostEqual(self.gutierre.compute_payout(), 3000.0)
     
     def test_employee_payout_with_comission_disabled(self):
         """Wheter payout is correctly computed in case of 10 contracts landed and 10 hours worked, but comission is disabled."""
-        raise NotImplementedError
+        self.gutierre.hours_worked = 10
+        self.gutierre.contracts_landed = 10
+        self.gutierre.has_comission = False
+        self.assertAlmostEqual(self.gutierre.compute_payout(), 2000.0)
